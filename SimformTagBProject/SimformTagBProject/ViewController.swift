2a0222869a4b45cccfbb33326c149bf08bfaff1b
Tosif Bhatti <tosif@simformsolutions.com>
2023-06-05 19:54:41 +0530

    Initial Setup

---
+++ SimformTagBProject/SimformTagBProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimformTagBProject
+//
+//  Created by Tosif Bhatti on 05/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
